7d052c11bda8ba3b1784a5cd18ceda4ed4d11334
Tugsem <saranlitugsem@gmail.com>
2022-04-28 16:17:32 +0300

    Add padding to the input elements

--- modules/methods.js
+++ modules/methods.js
@@ -1,7 +1,4 @@
 const ul = document.querySelector('#list');
-const add = document.querySelector('#add');
-const addIcon = document.querySelector('#add-icon');
-const clearChecked = document.querySelector('#clear');
 const storage = window.localStorage;
 
 const methods = {
@@ -75,42 +72,6 @@ const methods = {
     storage.setItem('tasks', JSON.stringify(taskArr));
     methods.changeTask();
   },
-};
-
-// add new task
-addIcon.onclick = () => {
-  const addedTask = JSON.parse(storage.getItem('tasks')) || [];
-  const { value } = add;
-  addedTask.push({ description: value, completed: false });
-  storage.setItem('tasks', JSON.stringify(addedTask));
-  const last = addedTask.length;
-  add.value = '';
-  methods.addTask(last);
-};
-
-// track changes on checkboxes
-ul.onclick = (e) => {
-  const tasksLS = JSON.parse(storage.getItem('tasks'));
-  if (e.target.type === 'checkbox') {
-    if (e.target.checked) {
-      e.target.parentNode.style.textDecoration = 'line-through';
-      tasksLS[e.target.parentNode.parentNode.id].completed = true;
-    } else {
-      e.target.parentNode.style.textDecoration = 'none';
-      tasksLS[e.target.parentNode.parentNode.id].completed = false;
-    }
-  }
-  storage.setItem('tasks', JSON.stringify(tasksLS));
-};
-// clear checked tasks from LS and html
-clearChecked.onclick = () => {
-  const tasksLS = JSON.parse(storage.getItem('tasks'));
-  const remains = tasksLS.filter((task) => task.completed === false);
-  remains.forEach((task, index) => {
-    task.index = ++index; // eslint-disable-line
-  });
-  storage.setItem('tasks', JSON.stringify(remains));
-  methods.displayTasks();
 };
 
 export default methods;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,46 @@
 import './style.css';
 import methods from '../modules/methods.js';
 
+const ul = document.querySelector('#list');
+const add = document.querySelector('#add');
+const addIcon = document.querySelector('#add-icon');
+const clearChecked = document.querySelector('#clear');
+const storage = window.localStorage;
+
+// add new task
+addIcon.onclick = () => {
+  const addedTask = JSON.parse(storage.getItem('tasks')) || [];
+  const { value } = add;
+  addedTask.push({ description: value, completed: false });
+  storage.setItem('tasks', JSON.stringify(addedTask));
+  const last = addedTask.length;
+  add.value = '';
+  methods.addTask(last);
+};
+
+// track changes on checkboxes
+ul.onclick = (e) => {
+  const tasksLs = JSON.parse(storage.getItem('tasks'));
+  if (e.target.type === 'checkbox') {
+    if (e.target.checked === true) {
+      e.target.parentNode.style.textDecoration = 'line-through';
+      tasksLs[e.target.parentNode.parentNode.id].completed = true;
+    } else {
+      e.target.parentNode.style.textDecoration = 'none';
+      tasksLs[e.target.parentNode.parentNode.id].completed = false;
+    }
+  }
+  storage.setItem('tasks', JSON.stringify(tasksLs));
+};
+// clear checked tasks from LS and html
+clearChecked.onclick = () => {
+  const tasksLS = JSON.parse(storage.getItem('tasks'));
+  const remains = tasksLS.filter((task) => task.completed === false);
+  remains.forEach((task, index) => {
+    task.index = ++index; // eslint-disable-line
+  });
+  storage.setItem('tasks', JSON.stringify(remains));
+  methods.displayTasks();
+};
+
 window.onload = methods.displayTasks();
