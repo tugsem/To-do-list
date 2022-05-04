075730836f77fc8ba121ca50f0a137fba5fa38d9
Tugsem <saranlitugsem@gmail.com>
2022-05-04 15:02:37 +0300

    Add keypress functionality

--- src/index.js
+++ src/index.js
@@ -7,15 +7,23 @@ const addIcon = document.querySelector('#add-icon');
 const storage = window.localStorage;
 
 // add new task
-addIcon.onclick = () => {
-  const addedTask = JSON.parse(storage.getItem('tasks')) || [];
-  const { value } = add;
-  addedTask.push({ description: value, completed: false });
-  storage.setItem('tasks', JSON.stringify(addedTask));
-  const last = addedTask.length;
-  add.value = '';
-  methods.addTask(last);
+const addItem = () => {
+  if (add.value.length > 0) {
+    const addedTask = JSON.parse(storage.getItem('tasks')) || [];
+    const { value } = add;
+    addedTask.push({ description: value, completed: false });
+    storage.setItem('tasks', JSON.stringify(addedTask));
+    const last = addedTask.length;
+    add.value = '';
+    methods.addTask(last);
+  }
 };
+addIcon.onclick = addItem;
+add.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    addItem();
+  }
+});
 
 window.onload = methods.displayTasks();
 window.onload = checkStatus();
